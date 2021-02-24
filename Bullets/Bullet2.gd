5c95dfc114be688f490c089d1aa62494a909e910
Dudewhothings <rabbidblaster@gmail.com>
2021-02-24 14:34:49 -0500

    Update

---
+++ Bullets/Bullet2.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var speed = 1
+var velocity = Vector2.ZERO
+
+func _physics_process(_delta):
+	position += velocity
+	velocity.y -= speed 
+	if position.y < -20:
+		queue_free()
+
+func _on_Timer_timeout():
+	queue_free()
